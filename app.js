a69a468d545228ad5a071139f05eb7b3d3ab355f
yukishige oba <yukishige.o.ru@gmail.com>
2020-07-08 10:09:00 +0900

    factorial関数を実装

--- app.js
+++ app.js
@@ -8,8 +8,12 @@
 function factorial(n) {
     let result = 1;
     // TODO このコメントを消して正しく実装してください。
+    if(n > 0){
+        result = n * factorial(n - 1);
+    }
     return result;
 }
+// console.log(factorial(100));
 const assert = require('assert');
 assert.equal(factorial(1), 1, `1の階乗は1ですが、実際は${factorial(1) }でした`);
 assert.equal(factorial(2), 2, `2の階乗は2ですが、実際は${factorial(2) }でした`);
